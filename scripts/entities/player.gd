7ce150efb98d219cabefe1dd20dcfac1c6a4850d
Robin Phillips <robinphillips462@gmail.com>
2025-03-03 12:46:42 -0500

    Implemented double jump.

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody3D
 
-
+#Movement related
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var mouse_sensitivity = 0.005
+var double_jumped: bool = false
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
@@ -13,12 +14,17 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	# Handle jump and double jump.
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif not is_on_floor() and not double_jumped:
+			velocity.y = JUMP_VELOCITY
+			double_jumped = true
+	if double_jumped and is_on_floor():
+		double_jumped = false
+	
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
