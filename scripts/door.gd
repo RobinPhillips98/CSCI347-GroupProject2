5086d9c9d679892f9d22a2ac5f46588963831c25
dsims <dsims2888@aol.com>
2025-03-20 17:00:34 -0400

    working on door

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,29 +1,25 @@
 extends Node3D
 
+@onready var door_animation = $AnimationPlayer
+@onready var area = $Area3D
+@onready var player = get_node()
 var has_key = false
 var door_open = false
 
-# Reference to the door animation
-#@onready var door_animation = $AnimationPlayer
 
-# Connect to the player's key variable (we'll get that from the player scene)
-var player = null
+#func _ready():
+	#player = get_parent().get_node("Player")
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player") and body.has_key:
 		open_door()
 
-func _ready():
-	player = get_parent().get_node("Player")
-
 func _process(delta):
 	if is_player_nearby() and has_key and !door_open:
 		open_door()
 		
-func is_player_nearby() -> bool: # Check if the player is in range (you can adjust the range here)
-		return global_transform.origin.distance_to(player.global_transform.origin) < 5
 
 func open_door():
 	door_open = true
-	#door_animation.play("open")  # Play the door's opening animation
-	# Optionally, you could add more logic here to prevent the door from closing again
+	$CollisionShape3D.disabled = true
+	door_animation.play("door_animation")
