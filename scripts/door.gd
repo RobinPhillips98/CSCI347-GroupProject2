2da5e357ba01389e790647e92cde2355623e9f47
dsims <dsims2888@aol.com>
2025-03-20 17:38:15 -0400

    finishing up

--- scripts/door.gd
+++ scripts/door.gd
@@ -2,22 +2,16 @@ extends Node3D
 
 @onready var door_animation = $AnimationPlayer
 @onready var area = $Area3D
-@onready var player = get_node()
+@onready var player = get_node("/root/World/Player")
 var has_key = false
 var door_open = false
 
-
-#func _ready():
-	#player = get_parent().get_node("Player")
-
-func _on_body_entered(body: Node3D) -> void:
-	if body.is_in_group("player") and body.has_key:
-		open_door()
-
 func _process(delta):
-	if is_player_nearby() and has_key and !door_open:
+	if Input.is_action_pressed("door_open") and player.has_key and is_player_nearby() and !door_open:
 		open_door()
 		
+func is_player_nearby() -> bool: # Check if the player is in range 
+		return global_transform.origin.distance_to(player.global_transform.origin) < 5
 
 func open_door():
 	door_open = true
